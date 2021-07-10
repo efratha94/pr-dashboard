2207493c37fdcc43639fef3ba823ffbb32f1ee82
Efrat Harel <efratha94@gmail.com>
2021-07-10 16:55:03 +0300

    regexing the description

--- src/client/components/FilteringTable.js
+++ src/client/components/FilteringTable.js
@@ -70,7 +70,8 @@ const FilteringTable = () => {
 
     useEffect(() => {
         const fetchPRs = async () => {
-            const listOfPRs = await axios.get("https://api.github.com/repos/nodejs/node/pulls?state=all")
+            // should be dynamic!
+            const listOfPRs = await axios.get("https://api.github.com/repos/facebook/react/pulls?state=all")
             
             if (listOfPRs.status !== 200) throw Error(listOfPRs.message);
 
--- src/client/services/preparePRs.js
+++ src/client/services/preparePRs.js
@@ -3,13 +3,14 @@
 const formattedPRs = (prArr) => {
 
     return prArr.map(pr => {
-        let splittedBody = pr.body.split("-->")
-  
+        // let splittedBody = pr.body
+        // .split("-->")
+        // console.log("pr", pr.number, pr.body)
         return {
             "pr_creation_date": pr.created_at,
             "pr_number": pr.number,
             "pr_title": pr.title,
-            "pr_description": splittedBody[1],
+            "pr_description": pr.body, //splittedBody[1],
             "pr_status": pr.draft === true ? "draft" : pr.state,
             "pr_labels": pr.labels,
             "pr_author": {username: pr.user.login, avatar: pr.user.avatar_url}
